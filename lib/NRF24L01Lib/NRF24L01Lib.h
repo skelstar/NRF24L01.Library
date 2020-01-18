3ccce3fa295b6dd814bcc216832fa085f63e4334
Sean <skelstar@gmail.com>
2020-01-18 14:38:06 +1300

    Added tests, moved lib files into lib folder, returning num retries now.

--- lib/NRF24L01Lib/NRF24L01Lib.h
+++ lib/NRF24L01Lib/NRF24L01Lib.h
@@ -4,7 +4,6 @@
 #include <Arduino.h>
 #include <RF24.h>
 #include <RF24Network.h>
-#include <VescData.h>
 
 #include <esp_int_wdt.h>
 #include <esp_task_wdt.h>
@@ -26,7 +25,7 @@ class NRF24L01Lib
 				PacketAvailableCallback packetAvailableCallback);
 		void update();
 		void read_into(uint8_t *data, uint8_t data_len);
-		bool send_with_retries(uint16_t to, uint8_t type, uint8_t *data, uint8_t data_len, uint8_t num_retries);
+		uint8_t send_with_retries(uint16_t to, uint8_t type, uint8_t *data, uint8_t data_len, uint8_t num_retries);
 		bool send_packet(uint16_t to, uint8_t type, uint8_t *data, uint8_t data_len);
 
 	private:
